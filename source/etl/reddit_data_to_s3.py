1f2fed73818dddbc1733c851d6484aaa68b01ae9
EC2 Default User <ec2-user@ip-172-31-16-223.us-west-2.compute.internal>
2019-07-21 16:49:12 +0000

    added function to get reddit access token

--- source/etl/reddit_data_to_s3.py
+++ source/etl/reddit_data_to_s3.py
@@ -1,7 +1,35 @@
 import requests
-
+import requests.auth
+import r_credentials
 
 ROOT_URL = "https://www.reddit.com"
 
 
+#get access token
+def get_r_token(username, password, appClientId, appSecret):
+	url = 'https://www.reddit.com/api/v1/access_token'
+	clientAuth = requests.auth.HTTPBasicAuth(appClientId, appSecret)
+	postData = {'grant_type': 'password', 'username': username, 'password': password}
+	headers = {'User-Agent': 'ChangeMeClient/0.1 by YourUserName'}
+	
+	print(('Retrieving access_token for user: {}').format(username))
 
+	try:
+		res = requests.post(url, auth=clientAuth,data=postData, headers=headers)
+		#print(res.json())
+		token = res.json()['access_token']
+		print(('Retrieved token: {}').format(token))
+		return token
+	except:
+		print('Could not retrive token')
+		raise
+		return 1
+	
+
+if __name__ == '__main__':
+	username = r_credentials.reddit_credentials['username']
+	password = r_credentials.reddit_credentials['password']
+	appClientId = r_credentials.reddit_credentials['appClientId']
+	appSecret = r_credentials.reddit_credentials['appSecret']
+	
+	token = get_r_token(username, password, appClientId, appSecret)
